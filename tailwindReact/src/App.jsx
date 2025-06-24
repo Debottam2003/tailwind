2db02fe8776e3cb67a8032fbbe432d265ff1720e
Debottam Kar <debottamkar2003@gmail.com>
2025-06-24 10:20:45 +0530

    tailwind

---
+++ tailwindReact/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+
+  return (
+    <>
+    <h1 className="bg-blue-800 text-amber-50 p-2">Hello Tailwind</h1>
+    <h2 className="bg-blue-700 text-amber-50 p-2">Hello Tailwind</h2>
+    <h3 className="bg-blue-600 text-amber-50 p-2">Hello Tailwind</h3>
+    <h4 className="bg-blue-500 text-amber-50 p-2">Hello Tailwind</h4>
+    <h5 className="bg-blue-400 text-amber-50 p-2">Hello Tailwind</h5>
+    <h6 className="bg-blue-300 text-amber-50 p-2">Hello Tailwind</h6>
+    </>
+  )
+}
+
+export default App
